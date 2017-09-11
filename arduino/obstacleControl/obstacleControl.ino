550a19ebb0a0a5c34c9609489a729ad9b2d5a1d3
rwarren2163 <richardwarren2163@gmail.com>
2017-09-11 11:41:04 -0400

    modified code for arduino due

--- arduino/obstacleControl/obstacleControl.ino
+++ arduino/obstacleControl/obstacleControl.ino
@@ -1,13 +1,10 @@
-#include <digitalWriteFast.h>
-
-
 // USER SETTINGS
 
 // pin assignments
 const int stepPin = 4;
 const int stepDirPin = 5;
-const int encoderPinA = 2; // encoder pins must be set to 2 and 3 for Arduino Uno because hardware interrupts exist on these pins
-const int encoderPinB = 3;
+const int encoderPinA = 20; // don't change encoderPins, because direct port manipulation is used that assumes 20 and 21 are used for encoder pins
+const int encoderPinB = 21;
 const int waterPin = 7;
 const int obstaclePin = 13; // signals whether the obstacle is engaged... this is sent to an arduino that controls the obstacle servo
 const int motorOffPin = 8; // turns on stepper motor driver
@@ -16,25 +13,24 @@ const int stopLimitPin = 10; // signal is LOW when engaged
 const int servoSwingTime = 200; // ms, approximate amount of time it takes for the osbtacle to pop out // this is used as a delay bewteen the obstacle reaching the end of the track and it coming back, to avoid it whacking the guy in the butt!
 
 // other user settings
+volatile int state = 3; // 1: no platform movement, no obstaacles, 2: platform movement, no obstacles, 3: platform movement and obstacles
 const int speedLookupLength = 100;
-const float rampResolution = .05; // < 1, smaller values are longer ramps
+const float rampResolution = .1; // < 1, smaller values are longer ramps
 volatile int stepperDelays[speedLookupLength];
-
-
-volatile int state = 2; // 1: no platform movement, no obstaacles, 2: platform movement, no obstacles, 3: platform movement and obstacles
 const float rewardRotations = 6;
 const int microStepping = 16; // only (1/microStepping) steps per pulse // this should correspond to the setting on the stepper motor driver, which is set by 3 digital inputs
 const int endPositionBuffer = 50 * microStepping; // motor stops endPositionBuffer steps before the beginning and end of the track
 const int waterDuration = 80; // milliseconds
-const long maxStepperSpeed = 12000 * long(microStepping); // (Hz) servo will move this fast to desired positions // maximum, unloaded appears to be around 2000 // NOTE: right now even a delay of microsecond fails to keep up with very fast speeds, so execution of other code in takeStep is rate limiting factor...
+const int maxStepperRPS = 12;
 const int motorSteps = 200;
 const int encoderSteps = 2880; // 720cpr * 4
 const int timingPulleyRad = 15.2789; // mm
 const float wheelRad = 95.25;
 int obstacleLocations[] = {2*encoderSteps, 4*encoderSteps, rewardRotations*encoderSteps*2}; // expressed in wheel ticks // the last element is a hack... the index goes up and the wheel position will never reach the last value, which is the desired behavior
-volatile float slowSpeedMultiplier = .25;
+volatile float slowSpeedMultiplier = .4;
 
 // initializations
+const long maxStepperSpeed = maxStepperRPS * 200 * long(microStepping);
 volatile int wheelTicks = 0;
 volatile int wheelTicksTemp = 0; // this variable temporarily copies wheelTicks in the main code to avoid having to access it multiple times, potentially colliding with its access in the interrupt
 volatile int obstacleInd = 0; // keeps track of which obstacle is being delivered for each reward trial
@@ -66,11 +62,11 @@ void setup() {
   pinMode(startLimitPin, INPUT_PULLUP);
   pinMode(stopLimitPin, INPUT_PULLUP);
   
-  digitalWriteFast(stepPin, LOW);
+  digitalWrite(stepPin, LOW);
   digitalWrite(stepDirPin, stepDir);
-  digitalWriteFast(waterPin, LOW);
-  digitalWriteFast(motorOffPin, LOW);
-  digitalWriteFast(obstaclePin, LOW);
+  digitalWrite(waterPin, LOW);
+  digitalWrite(motorOffPin, LOW);
+  digitalWrite(obstaclePin, LOW);
 
   // initialize encoder hhardware interrupt
   attachInterrupt(digitalPinToInterrupt(encoderPinA), encoder_isr, CHANGE);
@@ -84,10 +80,12 @@ void setup() {
   pulseDuration = stepperDelays[speedLookupLength];
 
   // begin serial communication
-  Serial.begin(1200);
+  Serial.begin(9600);
+//  delay(1000);
   Serial.println("1: rewards only");
   Serial.println("2: platform, no obstacles");
   Serial.println("3: platform, with obstacles");
+  Serial.print("min stepper delay: ");
   Serial.println(stepperDelays[speedLookupLength-1]);;
   
   // initialize track limits and move to starting position
@@ -96,7 +94,7 @@ void setup() {
   getStartLimit();
   stepperDelayInd = 0;
   takeStep(stepperStartPosition, slowSpeedMultiplier); // move back to stepperStartPosition
-  digitalWriteFast(motorOffPin, HIGH);
+  digitalWrite(motorOffPin, HIGH);
 }
 
 
@@ -117,18 +115,18 @@ void loop(){
       
       if ((state==2) || (state==3)){
         obstacleEngaged = true;
-        digitalWriteFast(motorOffPin, LOW); // engages stepper motor driver
+        digitalWrite(motorOffPin, LOW); // engages stepper motor driver
         stepperDelayInd = 0; // start at lowest velocity
       }
       
       if (state==3){
-        digitalWriteFast(obstaclePin, HIGH); // engages servo motor
+        digitalWrite(obstaclePin, HIGH); // engages servo motor
       }
     }
   // disengage:
   }else if (stepperTicks >= stepperStopPosition){
     obstacleEngaged = false;
-    digitalWriteFast(obstaclePin, LOW);
+    digitalWrite(obstaclePin, LOW);
     obstacleInd++;
     
     // return stage to starting position
@@ -136,7 +134,7 @@ void loop(){
     getStartLimit();
     stepperDelayInd = 0;
     takeStep(stepperStartPosition, slowSpeedMultiplier);
-    digitalWriteFast(motorOffPin, HIGH); // disengage stepper motor driver
+    digitalWrite(motorOffPin, HIGH); // disengage stepper motor driver
   }
      
 
@@ -148,9 +146,9 @@ void loop(){
     interrupts();
 
     obstacleInd = 0;
-    digitalWriteFast(waterPin, HIGH);
+    digitalWrite(waterPin, HIGH);
     delay(waterDuration);
-    digitalWriteFast(waterPin, LOW);
+    digitalWrite(waterPin, LOW);
 
   }
   
@@ -183,19 +181,21 @@ void takeStep(int stepsToTake, float speedMultiplier){
 
   for (int i = 0; i < abs(stepsToTake); i++){
 
-    // the two initial computation below take ~60 microseconds, thus rate limiting step speed...
-    // also, microseconds of stepPin pulse near 1, so in danger of being to short for driver?
+//    long micros1 = micros();
     
     // increment motor speed
     stepperDelayInd = min(stepperDelayInd + rampResolution, speedLookupLength-1);
 
     // get speed index
-    stepDelay = stepperDelays[int(stepperDelayInd)] / speedMultiplier;
+    stepDelay = stepperDelays[round(stepperDelayInd)] / speedMultiplier;
+
+//    long micros2 = micros();
+//    Serial.println(micros2 - micros1);
     
     // take step
-    digitalWriteFast(stepPin, HIGH);
+    digitalWrite(stepPin, HIGH);
     delayMicroseconds(stepDelay);
-    digitalWriteFast(stepPin, LOW);
+    digitalWrite(stepPin, LOW);
     delayMicroseconds(stepDelay);
   }
 
@@ -211,7 +211,8 @@ void encoder_isr() {
     static uint8_t enc_val = 0;
     
     enc_val = enc_val << 2;
-    enc_val = enc_val | ((PIND & 0b1100) >> 2);
+//    enc_val = enc_val | ((PIND & 0b1100) >> 2); this work for arduino uno if encoder pins are set to 2 and 3
+    enc_val = enc_val | ((REG_PIOB_PDSR & (0b11 << 12)) >> 12); // read all pins from port B, and bitmask to get just inputs 12 and 13 from port B, which correspond to Due pins 20 and 21
  
     wheelTicks = wheelTicks - lookup_table[enc_val & 0b1111];
 }
@@ -223,7 +224,7 @@ void getStartLimit(){
   stepperDelayInd = 0; // start at lowest velocity
   
   // find start limit
-  while (digitalReadFast(startLimitPin)){
+  while (digitalRead(startLimitPin)){
     takeStep(-1, slowSpeedMultiplier);
   }
   stepperTicks = 0;
@@ -237,7 +238,7 @@ void getEndLimit(){
   stepperDelayInd = 0; // start at lowest velocity
 
   // find stop limit
-  while (digitalReadFast(stopLimitPin)){
+  while (digitalRead(stopLimitPin)){
     takeStep(1, slowSpeedMultiplier);
   }
   stepperStopPosition = stepperTicks - endPositionBuffer;
