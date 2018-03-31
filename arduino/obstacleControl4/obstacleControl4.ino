0c2c306e78a602cb0a26c91004d9f09c62c86003
rwarren2163 <richardwarren2163@gmail.com>
2018-03-31 18:15:42 -0400

    wip

--- arduino/obstacleControl4/obstacleControl4.ino
+++ arduino/obstacleControl4/obstacleControl4.ino
@@ -19,19 +19,19 @@
 // user settings
 volatile int state = 3; // 1: no platform movement, no obstaacles, 2: platform movement, no obstacles, 3: platform movement and obstacles
 const int servoSwingTime = 200; // ms, approximate amount of time it takes for the osbtacle to pop out // this is used as a delay bewteen the obstacle reaching the end of the track and it coming back, to avoid it whacking the guy in the butt!
-const float acceleration = 10.0; // (m/(s^2))
 volatile float rewardRotations = 9.01;
 const int startPositionMm = 5;
 const int endPositionMm = 25;
-const float minSpeed = .01; // (m/s) can't use zero speed, b/c corresponding delay is inf, so start motor off at minSpeed
 const int waterDuration = 80; // milliseconds
 const double maxStepperSpeed = 1.5; // (m/s)
-volatile float callibrationSpeed = .6; // speed with which motor moves platform during callibration (m/s)
+const float acceleration = 6.0; //(m/s^2)
+volatile float callibrationSpeed = .6; // speed with motor moves plastform during callibration (m/s)
 const float obstacleLocations[] = {1.5, 4.5, 7.5, rewardRotations*20}; // expressed in wheel ticks // the last element is a hack... the index goes up and the wheel position will never reach the last value, which is the desired behavior
-const int velocitySamples = 10; // average velocity of wheel over this many samples // each sample last about 500 microseconds
+const int velocitySamples = 10; // each sample last about 500 microseconds
 const int obsPosJitter[] = {-100, 100}; // jitter range for the onset position of obstacles (mm)
 const int startPosJitter = 20; // (mm)
 const float obsLightProbability = 0.5;
+const long delayLookupLength = 20000;
 
 
 // rig characteristics
@@ -40,16 +40,15 @@ const int motorSteps = 200;
 const int encoderSteps = 2880; // 720cpr * 4
 const double timingPulleyRad = 15.2789; // (mm)
 const double wheelRad = 95.25; // (m)
-const long tableLength = 20000;
 
 
 // initializations
+const float mmPerMotorTic = ((2*PI*timingPulleyRad) / (motorSteps*microStepping));
 const int startPositionBuffer = startPositionMm * ((motorSteps*microStepping) / (2*PI*timingPulleyRad));
-const double motorTicsPerMm = (motorSteps*microStepping* 1000) / (2*PI*timingPulleyRad);
 const int endPositionBuffer = endPositionMm  * ((motorSteps*microStepping) / (2*PI*timingPulleyRad));; // motor stops endPositionBuffer steps before the beginning and end of the track
+volatile int stepperDelays[delayLookupLength];
 char* conditionNames[] = {"rewards only", "platform, no obstacles", "platform, with obstacles"};
 volatile int userInput;
-volatile int stepperDelays[tableLength];
 volatile int wheelTicks = 0;
 volatile int wheelTicksTemp = 0; // this variable temporarily copies wheelTicks in the main code to avoid having to access it multiple times, potentially colliding with its access in the interrupt
 volatile int obstacleInd = 0; // keeps track of which obstacle is being delivered for each reward trial
@@ -61,19 +60,20 @@ volatile int rewardPosition = rewardRotations * encoderSteps; // expressed in wh
 const double conversionFactor = (wheelRad / timingPulleyRad) * (float(motorSteps) / encoderSteps) * microStepping; // this converts from analogRead reading of wheel encoder to desired number of steps in stepper driver
 volatile bool stepDir = HIGH;
 volatile bool obstacleEngaged = false;
-volatile float stepSpeed;
-volatile float targetSpeed;
-volatile float stepDelay;
-volatile float targetStepSpeed;
+volatile long stepperDelayInd = 0;
+volatile long maxStepperDelayInd;
+volatile int stepDelay;
 volatile long currentMicros = 0;
 volatile long lastMicros = 0;
 volatile int deltaMicros = 0;
+volatile int targetStepDelay = 0;
 volatile int callibrationDelay; // tbd in setup
 volatile int maxSpeedDelay; // tbd in setup
 volatile int startingWheelTics = 0;
 volatile int startingStepperTics = 0;
 volatile int obstacleLocationSteps[sizeof(obstacleLocations)];
 volatile int obsPos; // position of obstacle, in wheel tics, on a given trial
+volatile float stepperSpeed;
 RunningMedian deltaMicroSmps = RunningMedian(5);
 
 
@@ -102,13 +102,6 @@ void setup() {
 
   // initialiez random seed
   randomSeed(analogRead(0));
-
-
-  // initialize lookup table for stepper speeds
-  for (int i=0; i<tableLength; i++){
-    stepperDelays[i] = 500;
-  }
-  callibrationDelay = getMotorDelayFromSpeed(callibrationSpeed);
   
 
   // initialize encoder hardware interrupt
@@ -121,9 +114,22 @@ void setup() {
   printMenuAndSettings();
   
   
-  // initialize delays for max and callibration speeds
+  // initialize lookup table for stepper speeds
+  stepperSpeed = 0;
+  for (int i=0; i<delayLookupLength; i++){
+    stepperSpeed = 0.5 * (stepperSpeed + sqrt(4*acceleration*mmPerMotorTic/1000 + pow(stepperSpeed,2)));
+    stepperDelays[i] = getMotorDelayFromSpeed(stepperSpeed);
+    if (stepperSpeed>maxStepperSpeed){
+      maxStepperDelayInd = i;
+      break;
+    }
+  }
   callibrationDelay = getMotorDelayFromSpeed(callibrationSpeed);
   maxSpeedDelay = getMotorDelayFromSpeed(maxStepperSpeed);
+  if (maxStepperDelayInd==0){
+    Serial.println("WARNING: Too many values in stepper delay lookup!");
+    Serial.println("Try increasing acceleration or decreasing max stepper velocity...");
+  }
 
   
   // initialize locations of obstacles
@@ -215,21 +221,21 @@ void loop(){
 
 
 
-// begin tracking by accelerating obs until speed matches that of wheel
+// move stepper one step in stepDirection
 void startTracking(){
 
   // set motor direction to forward
   digitalWrite(stepDirPin, HIGH);
-  delayMicroseconds(1); // TCM2100 stepper motor driver requires 20 nanoseconds between stepDirectionPin change and stepPin input // the driver was messing up occasionally, so hopefully this will fix the problem
 
-  targetSpeed = getSpeedFromWheelDelay(deltaMicroSmps.getAverage());
-  setMotorSpeedToMin();
+  targetStepDelay = getMotorDelayFromWheelDelay(deltaMicroSmps.getAverage());
+  stepperDelayInd = 0;
+  stepDelay = stepperDelays[0];
 
-  while (stepSpeed < targetSpeed){
+  while (stepDelay >= targetStepDelay){
     
-    // get step speed and delay
-    stepSpeed = constrain(stepSpeed + acceleration*stepDelay*pow(10,-6), 0, targetSpeed);
-    stepDelay = getMotorDelayFromSpeed(stepSpeed);
+    // increment motor speed
+    stepperDelayInd = min(stepperDelayInd+1, maxStepperDelayInd);
+    stepDelay = max(stepperDelays[stepperDelayInd], targetStepDelay);
 
     // take step
     digitalWrite(stepPin, HIGH);
@@ -239,7 +245,7 @@ void startTracking(){
     stepperTicks++;
 
     // update target delay based on current wheel speed
-    targetSpeed = getSpeedFromWheelDelay(deltaMicroSmps.getAverage());
+    targetStepDelay = getMotorDelayFromWheelDelay(deltaMicroSmps.getAverage());
   }
 
   // record wheel and motor positions at the start of positional tracking  
@@ -267,8 +273,9 @@ void takeStep(int stepsToTake){
   digitalWrite(stepDirPin, (stepsToTake>0));
   delayMicroseconds(1); // TCM2100 stepper motor driver requires 20 nanoseconds between stepDirectionPin change and stepPin input // the driver was messing up occasionally, so hopefully this will fix the problem
 
-  // take steps
   for (int i = 0; i < abs(stepsToTake); i++){
+
+    // take step
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(maxSpeedDelay);
     digitalWrite(stepPin, LOW);
@@ -281,7 +288,7 @@ void takeStep(int stepsToTake){
 
 
 // move stepper one step in stepDirection
-void takeAcceleratingStep(int stepsToTake, float acceleration, float minSpeed, float maxSpeed){
+void takeAcceleratingStep(int stepsToTake, int accelDirection, int minDelay, int maxDelay){
 
   // set motor direction
   digitalWrite(stepDirPin, (stepsToTake>0));
@@ -289,10 +296,12 @@ void takeAcceleratingStep(int stepsToTake, float acceleration, float minSpeed, f
 
   for (int i = 0; i < abs(stepsToTake); i++){
 
-    // get motor speed and delay
-    stepSpeed = constrain(stepSpeed + acceleration*stepDelay*pow(10,-6), minSpeed, maxSpeed);
-//    Serial.println(stepSpeed + acceleration*stepDelay*pow(10,-6));
-    stepDelay = getMotorDelayFromSpeed(stepSpeed);
+    // increment motor speed
+    if ((stepperDelays[stepperDelayInd]>=minDelay) && (stepperDelays[stepperDelayInd]<=maxDelay)){
+      stepperDelayInd = stepperDelayInd + accelDirection;
+      stepDelay = constrain(stepperDelays[stepperDelayInd], minDelay, maxDelay);
+    }
+    
 
     // take step
     digitalWrite(stepPin, HIGH);
@@ -303,6 +312,11 @@ void takeAcceleratingStep(int stepsToTake, float acceleration, float minSpeed, f
 
   stepperTicks += stepsToTake;
 }
+
+
+
+
+
 
 
 
@@ -328,11 +342,11 @@ void encoder_isr() {
 
 void getStartLimit(){
 
-  setMotorSpeedToMin(); // start at lowest velocity
+  stepperDelayInd = 0; // start at lowest velocity
   
   // find start limit
   while (digitalRead(startLimitPin)){
-    takeAcceleratingStep(-1, acceleration, 0, callibrationSpeed);
+    takeAcceleratingStep(-1, 1, callibrationDelay, stepperDelays[0]);
   }
   stepperTicks = 0;
   
@@ -342,11 +356,11 @@ void getStartLimit(){
 
 void getEndLimit(){
 
-  setMotorSpeedToMin(); // start at lowest velocity
+  stepperDelayInd = 0; // start at lowest velocity
 
   // find stop limit
   while (digitalRead(stopLimitPin)){
-    takeAcceleratingStep(1, acceleration, 0, callibrationSpeed);
+    takeAcceleratingStep(1, 1, callibrationDelay, stepperDelays[0]);
   }
   stepperStopPosition = stepperTicks - endPositionBuffer;
 
@@ -356,12 +370,11 @@ void getEndLimit(){
 
 void initializeLimits(){
 
-  setMotorSpeedToMin();
   getStartLimit();
   getEndLimit();
   getStartLimit();
-  setMotorSpeedToMin();
-  takeAcceleratingStep(startPositionBuffer + getStartJitter(startPosJitter), acceleration, 0, callibrationSpeed); // move back to startPositionBuffer
+  stepperDelayInd = 0; // start at lowest velocity
+  takeAcceleratingStep(startPositionBuffer + getStartJitter(startPosJitter), 1, callibrationDelay, stepperDelays[0]); // move back to startPositionBuffer
   digitalWrite(motorOffPin, HIGH);
   
 }
@@ -384,8 +397,8 @@ void recalibrateLimits(){
   }
 
   // return to starting position
-  setMotorSpeedToMin();
-  takeAcceleratingStep(startPositionBuffer + getStartJitter(startPosJitter), acceleration, 0, callibrationSpeed); // move back to startPositionBuffer
+  stepperDelayInd = 0; // start at lowest velocity
+  takeAcceleratingStep(startPositionBuffer + getStartJitter(startPosJitter), 1, callibrationDelay, stepperDelays[0]); // move back to startPositionBuffer
   digitalWrite(motorOffPin, HIGH); // disengage stepper motor driver
   
 }
@@ -502,19 +515,9 @@ void giveReward(){
 
 
 
-void setMotorSpeedToMin(){
-
-  stepSpeed = minSpeed;
-  stepDelay = getMotorDelayFromSpeed(stepSpeed);;
-  
-}
-
-
-
-
 long getMotorDelayFromSpeed(float motorSpeed){
 
-    long ticsPerSecond = motorSpeed * motorTicsPerMm;
+    long ticsPerSecond = motorSpeed * (motorSteps*microStepping* 1000) / (2*PI*timingPulleyRad);
     int microSecondsPerTic = (1 / (ticsPerSecond / pow(10,6))) / 2;
     return microSecondsPerTic;
 
@@ -522,9 +525,9 @@ long getMotorDelayFromSpeed(float motorSpeed){
 
 
 
-double getSpeedFromWheelDelay(int wheelDelay){
+int getMotorDelayFromWheelDelay(int wheelDelay){
 
-  return ((2*PI*wheelRad)/encoderSteps) / (wheelDelay) * 1000; // speed of 
+  return (wheelDelay * (mmPerMotorTic / ((2*PI*wheelRad) / encoderSteps))) / 2;
   
 }
 
@@ -532,8 +535,8 @@ double getSpeedFromWheelDelay(int wheelDelay){
 
 int setObsPos(int index){
 
-  static double wheelTicsPerMm = (encoderSteps / (2*PI*wheelRad));
-  obsPos =  obstacleLocationSteps[index] + random(obsPosJitter[0]*wheelTicsPerMm, obsPosJitter[1]*wheelTicsPerMm); // initialize first position)
+  static double ticsPerMm = (encoderSteps / (2*PI*wheelRad));
+  obsPos =  obstacleLocationSteps[index] + random(obsPosJitter[0]*ticsPerMm, obsPosJitter[1]*ticsPerMm); // initialize first position)
 
 }
 
@@ -541,7 +544,7 @@ int setObsPos(int index){
 
 int getStartJitter(int jitterMax){
 
-  return random(0, motorTicsPerMm*jitterMax);
+  return random(0, jitterMax/mmPerMotorTic);
   
 }
 
