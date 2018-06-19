175505e220c7e686170b3b1c0def46d35f6ab1da
rwarren2163 <richardwarren2163@gmail.com>
2018-06-19 18:12:29 -0400

    wip

--- arduino/other/capacitiveTouch/capacitiveTouch.ino
+++ arduino/other/capacitiveTouch/capacitiveTouch.ino
@@ -1,4 +1,5 @@
 #include <CapacitiveSensor.h>
+#include <Filters.h>
 
 // pin assignments
 const int touchSendPin = 8;
@@ -6,13 +7,21 @@ const int touchReceivePin = 6;
 const int isTouchingPin = 12;
 
 // user settings
-const int touchThresh = 375; // 525;
+const int touchThresh = 720; // 525;
 const int sensorSmps = 2;
 const int maxMeasurementTime = 10; //2; // ms, capacitive touch reading times out after maxMeasurementTime
+const int baselineSmpNum = 10000;
+const int medianSmpNum = 19;
+const float filterFrequency = 10.0;
+const float baselineFilterFrequency = 2.0;
 
 // other initializations
 CapacitiveSensor touchSensor = CapacitiveSensor(touchSendPin, touchReceivePin);
+FilterOnePole lowpassFilter(LOWPASS, filterFrequency);
+FilterOnePole lowpassBaselineFilter(LOWPASS, baselineFilterFrequency);
 volatile int touchMeasurement = 0;
+volatile int touchMeasurementSmoothed = 0;
+volatile int baseline = 0;
 
 
 
@@ -24,7 +33,7 @@ void setup(){
 
   // setup touch sensor
   touchSensor.set_CS_Timeout_Millis(maxMeasurementTime);
-
+  
   Serial.begin(115200);
   
 }
@@ -39,11 +48,16 @@ void loop(){
   // if overflowed, set to max value
   if (touchMeasurement<0){
     touchMeasurement = touchThresh+1;
-  
   // otherwise normalize by number of sensor samples
   }else{
     touchMeasurement = touchMeasurement / sensorSmps;
   }
+
+  
+  // filter
+  touchMeasurementSmoothed = lowpassFilter.input(touchMeasurement);
+  baseline = lowpassBaselineFilter.input(touchMeasurement);
+  
   
   
   // set isTouchingPin 
@@ -53,7 +67,13 @@ void loop(){
     digitalWrite(isTouchingPin, LOW);
   }
 
-  Serial.println(touchMeasurement);
+  Serial.print(touchMeasurement);
+  Serial.print(",");
+  Serial.print(touchMeasurementSmoothed);
+  Serial.print(",");
+  Serial.print(baseline);
+  Serial.print(",");
+  Serial.println(abs(touchMeasurementSmoothed - baseline));
   delay(1);
 }
 
